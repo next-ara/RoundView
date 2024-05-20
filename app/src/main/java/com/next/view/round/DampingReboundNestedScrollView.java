158830baca92c0dc3dca277393ae80df07885348
Afton <next-era@foxmail.com>
2024-05-20 14:42:40 +0800

    修复DampingReboundNestedScrollView嵌套RecyclerView滑动，导致RecyclerView懒加载失效的问题

--- app/src/main/java/com/next/view/round/DampingReboundNestedScrollView.java
+++ app/src/main/java/com/next/view/round/DampingReboundNestedScrollView.java
@@ -6,11 +6,13 @@ import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.TranslateAnimation;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * ClassName:阻尼反弹嵌套滚动控件类
@@ -45,6 +47,12 @@ public class DampingReboundNestedScrollView extends NestedScrollView {
     //水平移动搞定距离
     private float moveHeight;
 
+    //列表控件对象的控件id
+    private int recyclerViewId;
+
+    //列表控件对象
+    private RoundRecyclerView recyclerView;
+
     public DampingReboundNestedScrollView(@NonNull Context context) {
         super(context);
     }
@@ -61,6 +69,42 @@ public class DampingReboundNestedScrollView extends NestedScrollView {
         this.setFillViewport(true);
         //初始化
         this.init(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        if (this.recyclerView != null) {
+            this.recyclerView.setMaxHeight(height);
+        } else {
+            this.findRecyclerView(this);
+            if (this.recyclerView != null) {
+                this.recyclerView.setMaxHeight(height);
+            }
+        }
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        if (this.recyclerView != null) {
+            View child = getChildAt(0);
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            int topHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin - getMeasuredHeight();
+            int scrollY = getScrollY();
+            boolean topIsShow = scrollY >= 0 && scrollY < topHeight;
+            if (topIsShow) {
+                int remainScrollY = topHeight - scrollY;
+                int selfScrollY = Math.min(remainScrollY, dy);
+                scrollBy(0, selfScrollY);
+                consumed[1] = selfScrollY;
+            } else {
+                super.onNestedPreScroll(target, dx, dy, consumed, type);
+            }
+        } else {
+            super.onNestedPreScroll(target, dx, dy, consumed, type);
+        }
     }
 
     @Override
@@ -83,8 +127,7 @@ public class DampingReboundNestedScrollView extends NestedScrollView {
                 this.previousY = this.startY;
 
                 //记录childView的初始位置
-                this.topRect.set(this.childView.getLeft(), this.childView.getTop(),
-                        this.childView.getRight(), this.childView.getBottom());
+                this.topRect.set(this.childView.getLeft(), this.childView.getTop(), this.childView.getRight(), this.childView.getBottom());
 
                 this.moveHeight = 0;
                 break;
@@ -95,7 +138,6 @@ public class DampingReboundNestedScrollView extends NestedScrollView {
 
                 //判定是否在顶部或者滑到了底部
                 if ((!this.childView.canScrollVertically(-1) && (this.currentY - this.startY) > 0) || (!this.childView.canScrollVertically(1) && (this.currentY - this.startY) < 0)) {
-                    //计算阻尼
                     float distance = this.currentY - this.startY;
                     if (distance < 0) {
                         distance *= -1;
@@ -121,8 +163,7 @@ public class DampingReboundNestedScrollView extends NestedScrollView {
 
                     this.moveHeight = this.moveHeight + (this.deltaY * damping);
 
-                    this.childView.layout(this.topRect.left, (int) (this.topRect.top + this.moveHeight), this.topRect.right,
-                            (int) (this.topRect.bottom + this.moveHeight));
+                    this.childView.layout(this.topRect.left, (int) (this.topRect.top + this.moveHeight), this.topRect.right, (int) (this.topRect.bottom + this.moveHeight));
                 }
                 break;
             case MotionEvent.ACTION_UP:
@@ -130,8 +171,7 @@ public class DampingReboundNestedScrollView extends NestedScrollView {
                     //开始回移动画
                     this.upDownMoveAnimation();
                     //子控件回到初始位置
-                    this.childView.layout(this.topRect.left, this.topRect.top, this.topRect.right,
-                            this.topRect.bottom);
+                    this.childView.layout(this.topRect.left, this.topRect.top, this.topRect.right, this.topRect.bottom);
                 }
 
                 this.startY = 0;
@@ -147,8 +187,7 @@ public class DampingReboundNestedScrollView extends NestedScrollView {
      * 开始回移动画
      */
     private void upDownMoveAnimation() {
-        TranslateAnimation animation = new TranslateAnimation(0.0f, 0.0f,
-                this.childView.getTop(), this.topRect.top);
+        TranslateAnimation animation = new TranslateAnimation(0.0f, 0.0f, this.childView.getTop(), this.topRect.top);
         animation.setDuration(600);
         animation.setFillAfter(true);
         //设置阻尼动画效果
@@ -163,12 +202,32 @@ public class DampingReboundNestedScrollView extends NestedScrollView {
      * @param attrs   属性
      */
     private void init(Context context, AttributeSet attrs) {
-        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.RoundView);
-        float radius = ta.getDimension(R.styleable.RoundView_radius, 0);
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.DampingReboundNestedScrollView);
+        float radius = ta.getDimension(R.styleable.DampingReboundNestedScrollView_radius, 0);
+        this.recyclerViewId = ta.getResourceId(R.styleable.DampingReboundNestedScrollView_recyclerViewId, 0);
         ta.recycle();
 
         //设置圆角半径
         this.setRadius(radius);
+    }
+
+    /**
+     * 递归查找列表控件
+     *
+     * @param view 父控件
+     */
+    private void findRecyclerView(View view) {
+        if (view instanceof ViewGroup viewGroup && !(view instanceof RecyclerView)) {
+            int childCount = viewGroup.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View childView = viewGroup.getChildAt(i);
+                this.findRecyclerView(childView);
+            }
+        } else {
+            if (view.getId() == this.recyclerViewId && view instanceof RoundRecyclerView) {
+                this.recyclerView = (RoundRecyclerView) view;
+            }
+        }
     }
 
     /**
