5bb7c58373857a68e3799fb0b4e2ae909c49e43c
Afton <next-era@foxmail.com>
2024-05-21 16:05:58 +0800

    新增圆角回弹布局

---
+++ app/src/main/java/com/next/view/round/RoundSpringBackLayout.java
@@ -0,0 +1,68 @@
+package com.next.view.round;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.next.view.springback.SpringBackLayout;
+
+/**
+ * ClassName:圆角回弹布局类
+ *
+ * @author Afton
+ * @time 2024/1/5
+ * @auditor
+ */
+public class RoundSpringBackLayout extends SpringBackLayout {
+
+    //圆角半径
+    private float radius = 0;
+
+    public RoundSpringBackLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public RoundSpringBackLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        //初始化
+        this.init(context, attrs);
+    }
+
+    /**
+     * 初始化
+     *
+     * @param context 上下文
+     * @param attrs   属性
+     */
+    private void init(Context context, AttributeSet attrs) {
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.RoundView);
+        float radius = ta.getDimension(R.styleable.RoundView_radius, 0);
+        ta.recycle();
+
+        //设置圆角半径
+        this.setRadius(radius);
+    }
+
+    /**
+     * 设置圆角半径
+     *
+     * @param radius 圆角半径
+     */
+    public void setRadius(float radius) {
+        if (this.radius == radius) {
+            return;
+        }
+
+        RoundOutlineProvider roundOutlineProvider = new RoundOutlineProvider(radius);
+        this.setOutlineProvider(roundOutlineProvider);
+        this.setClipToOutline(true);
+        this.radius = radius;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+}
